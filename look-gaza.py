e72b1e1d2c434f4040ed3952e45cef516230da0d
natso500 <49106380+natso500@users.noreply.github.com>
2019-05-10 17:59:00 +0300

    Add files via upload

---
+++ look-gaza.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import time
+os.system("figlet teak Cpnal")
+print "teak Cpnal [1]"
+print "Exit [2]"
+user=input ("===>>")
+if user == 1:
+ hello=raw_input ("enter  url :")
+os.system("clear")
+os.system(" figlet start teak")
+print "[            ] 0%"
+time.sleep(5)
+print "[===         ] 25%"
+time.sleep(5)
+print "[======      ] 50%"
+time.sleep(5)
+print "[=========   ] 75%"
+time.sleep(5)
+print "[============] 100%"
+time.sleep(3)
+helloworld = "naw cleak to the url ===>>>" + " " + hello + "" + ":2083"
+print helloworld
