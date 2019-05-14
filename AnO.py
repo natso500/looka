7d11740a18117be02a8a351affed95ebddeccab4
natso500 <49106380+natso500@users.noreply.github.com>
2019-05-14 20:23:14 +0300

    Add files via upload

---
+++ AnO.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import time
+R = '\033[31;1m'
+G = '\033[32;1m'
+W = '\033[39;1m'
+print                           
+print                           
+print                          
+print                           
+print                     
+print G
+print        "    -1 Dos-Attak                                          ".center(44)
+print      "     -2  teak Cpnal          \  (   )   /                      ".center(44)
+print     "       -3 Cut net            (  O   O  )               ".center(44)
+print   "                           (     {}       )              ".center(44)
+print W 
+S =input ( R + "Enter ===>>"  + W)
+if S == 2:
+ print
+ print
+ user=raw_input ("Enter url :  ")
+ os.system("clear")
+ print "pleas wait ... "
+ time.sleep(10)
+ print "%sThis is Cpnal  ===>>  %s %s:2083 %s" %(R, G, user, W)
+elif S == 1:
+ os.system("git clone https://github.com/natso500/NatSo.git")
+elif S == 3:
+ os.system("git clone https://github.com/natso500/CUUT_ALL.git")
+ 
+
+
+ 
+ 
